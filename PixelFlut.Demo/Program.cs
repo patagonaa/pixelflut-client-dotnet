1e51f4e132517926cd293e52fac8d8482427e9e5
Patrick Krämer <patrickkraemerhn@aol.com>
2018-03-20 22:13:39 +0100

    initial

---
+++ PixelFlut.Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using PixelFlut.Infrastructure;
+
+namespace PixelFlut.Demo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var r = new Random();
+            var ip = IPAddress.Parse("192.168.178.148");
+            using (var client = new Client())
+            {
+                await client.Connect(ip, 3141);
+                var tasks = new List<Task>();
+                var c = Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+                for (int y = 0; y < 256; y++)
+                {
+                    for (int x = 0; x < 256; x++)
+                    {
+                        client.SetPixel(x, y, (x / 4 + y / 4) % 2 == 1 ? c : Color.Black);
+                    }
+                    Console.WriteLine($"Line {y} written");
+                }
+            }
+            Console.WriteLine("Ended normally");
+        }
+    }
+}
---
+++ PixelFlut.Infrastructure/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelFlut.Infrastructure
+{
+    public class Client : IDisposable
+    {
+        private readonly Socket _socket;
+
+        public Client()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //_socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, true);
+        }
+
+        public async Task Connect(IPAddress ip, int port)
+        {
+            await _socket.ConnectAsync(ip, port);
+        }
+
+        public void Dispose()
+        {
+            _socket.Close();
+            _socket.Dispose();
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            var msg = $"PX {x} {y} {color.R:X2}{color.G:X2}{color.B:X2}";
+            if (color.A != 255)
+                msg += $"{color.A:X2}";
+            msg += "\n";
+            _socket.Send(Encoding.UTF8.GetBytes(msg));
+        }
+    }
+}
