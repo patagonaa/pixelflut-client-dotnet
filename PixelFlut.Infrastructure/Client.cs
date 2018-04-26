09c454314289146a8020c249d16f4772ac7a585d
Patrick Krämer <patrickkraemerhn@aol.com>
2018-04-27 00:19:40 +0200

    solitaire

--- PixelFlut.Infrastructure/Client.cs
+++ PixelFlut.Infrastructure/Client.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Net;
@@ -10,17 +11,26 @@ namespace PixelFlut.Infrastructure
 {
     public class Client : IDisposable
     {
-        private readonly Socket _socket;
+        private Socket _socket;
+        private readonly IPAddress _ip;
+        private readonly int _port;
 
-        public Client()
+        public Client(IPAddress ip, int port)
         {
             _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            //_socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.NoDelay, true);
+            _socket.SendBufferSize = 50 * 1024 * 1024;
+            _socket.SendTimeout = 1000;
+            _ip = ip;
+            _port = port;
         }
 
-        public async Task Connect(IPAddress ip, int port)
+        public void Connect()
         {
-            await _socket.ConnectAsync(ip, port);
+            _socket.Dispose();
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.SendBufferSize = 50 * 1024 * 1024;
+            _socket.SendTimeout = 1000;
+            _socket.Connect(_ip, _port);
         }
 
         public void Dispose()
@@ -29,26 +39,89 @@ namespace PixelFlut.Infrastructure
             _socket.Dispose();
         }
 
+        private object _msgLock = new object();
+        private StringBuilder _msg = new StringBuilder(100000000);
+
         public void SetPixel(int x, int y, Color color)
         {
-            var msg = $"PX {x} {y} {color.R:X2}{color.G:X2}{color.B:X2}";
-            if (color.A != 255)
-                msg += $"{color.A:X2}";
-            msg += "\n";
-            _socket.Send(Encoding.UTF8.GetBytes(msg));
+            lock (_msgLock)
+            {
+                _msg.Append("PX ");
+                _msg.Append(x);
+                _msg.Append(" ");
+                _msg.Append(y);
+                _msg.Append(" ");
+                _msg.Append(color.R.ToString("X2"));
+                _msg.Append(color.G.ToString("X2"));
+                _msg.Append(color.B.ToString("X2"));
+                if (color.A != 255)
+                    _msg.Append(color.A.ToString("X2"));
+                _msg.Append("\n");
+            }
+        }
+
+        public void Write()
+        {
+            try
+            {
+                string msg;
+
+                //lock (_msgLock)
+                {
+                    msg = _msg.ToString();
+                }
+
+                _socket.Send(Encoding.UTF8.GetBytes(msg));
+
+                //Console.Write(msg);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Connection fucked: {ex.SocketErrorCode}\n{ex}");
+                if (ex.SocketErrorCode == SocketError.Shutdown ||
+                    ex.SocketErrorCode == SocketError.ConnectionAborted ||
+                    ex.SocketErrorCode == SocketError.TimedOut)
+                {
+                    Connect();
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_msgLock)
+            {
+                _msg.Clear();
+            }
         }
 
         public Size GetSize()
         {
-            var bytes = new byte[1000];
+            while (true)
+            {
+                var bytes = new byte[1000];
+                int receivedBytes;
+                using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    socket.Connect(_ip, _port);
+                    socket.Send(Encoding.UTF8.GetBytes("SIZE\n"));
 
-            _socket.Send(Encoding.UTF8.GetBytes("SIZE\n"));
+                    receivedBytes = socket.Receive(bytes);
 
-            int receivedBytes = _socket.Receive(bytes);
-
-            var str = Encoding.UTF8.GetString(bytes, 0, receivedBytes);
-            var split = str.Split(' ');
-            return new Size(int.Parse(split[1]), int.Parse(split[2]));
+                    socket.Close();
+                }
+                var str = Encoding.UTF8.GetString(bytes, 0, receivedBytes);
+                //Console.WriteLine($"SIZE answer: {str}");
+                var split = str.Split(' ');
+                try
+                {
+                    return new Size(int.Parse(split[1]), int.Parse(split[2]));
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    continue;
+                }
+            }
         }
     }
 }
